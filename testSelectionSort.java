0d42eed9cabf86a9d121686fa49860793728fecd
Lubna <ltattan@umich.edu>
2020-12-01 19:48:08 -0500

    Adding mixed and duplicate functions

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -17,7 +17,7 @@ public class testSelectionSort {
     }
 
     public void testPositive(){
-      
+
     }
 
     public void testNegative(){
@@ -26,11 +26,34 @@ public class testSelectionSort {
 
 
     public void testMixed(){
+      int[] mixes_arr = new int[5];
+      mixes_arr[0] = -8;
+      mixes_arr[1] = 9;
+      mixes_arr[2] = 7;
+      mixes_arr[3] = 0;
+      mixes_arr[4] = -2;
+      SelectionSort selectionsort = new SelectionSort();
+      selectionsort.basicSelectionSort(mixes_arr);
+      for(int i = 0; i<mixes_arr.length;++i) {
+        System.out.println("Mixed Array: " + mixes_arr[i]);
+      }
 
         }
     }
 
     public void testDuplicates(){
+
+    	int[] duplicate_arr = new int[5];
+        duplicate_arr[0] = -8;
+        duplicate_arr[1] = 9;
+        duplicate_arr[2] = 7;
+        duplicate_arr[3] = 9;
+        duplicate_arr[4] = -8;
+        SelectionSort selectionsort = new SelectionSort();
+        selectionsort.basicSelectionSort(duplicate_arr);
+        for(int i = 0; i < duplicate_arr.length;++i) {
+        	System.out.println("Duplicate Array: " + duplicate_arr[i]);
+        }
 
     }
 
