f9ecc4335f6a8c79bcf6593678aa0ed333a571e3
Lubna <ltattan@umich.edu>
2020-12-01 19:41:27 -0500

    Adding to positive and negative functions

---
+++ testSelectionSort.java
@@ -0,0 +1,39 @@
+package lab10;
+
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class testSelectionSort {
+	@Test
+	public void test() {
+		testPositive();
+		testNegative();
+		testMixed();
+		testDuplicates();
+	}
+
+    public testSelectionSort() {
+    }
+
+    public void testPositive(){
+
+    }
+
+    public void testNegative(){
+
+    }
+
+
+    public void testMixed(){
+
+    }
+
+    public void testDuplicates(){
+
+
+
+    }
+
+
+}
